4e26b849c7e7fd9201da4f328dcb8bb19a5f26b1
Claudio <claudio.martella@gmail.com>
2011-09-20 18:02:10 +0200

    fixed licensing

--- src/main/java/org/acaro/graffiti/processing/GraffitiMessage.java
+++ src/main/java/org/acaro/graffiti/processing/GraffitiMessage.java
@@ -1,3 +1,18 @@
+/*  Copyright 2011 Claudio Martella
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
 package org.acaro.graffiti.processing;
 
 import java.io.DataInput;
@@ -33,9 +48,9 @@ public class GraffitiMessage
     
     @Override
     public void readFields(DataInput input) throws IOException {
-        this.query = new Query();
-        this.query.readFields(input);
+        this.query   = new Query();
         this.results = new ResultSet();
+        this.query.readFields(input);
         this.results.readFields(input);
     }
 
