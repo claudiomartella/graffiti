66366c2bc37a067b2811ac876fc49f9041e8b013
Claudio <claudio.martella@gmail.com>
2011-09-20 16:57:25 +0200

    Refactoring of Message

---
+++ src/main/java/org/acaro/graffiti/processing/GraffitiMessage.java
@@ -0,0 +1,46 @@
+package org.acaro.graffiti.processing;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.acaro.graffiti.query.Query;
+import org.apache.hadoop.io.Writable;
+
+public class GraffitiMessage implements Writable, Cloneable {
+
+    private Query query;
+    private ResultSet results;
+
+    public GraffitiMessage(Query query, ResultSet results) {
+        this.query   = query;
+        this.results = results;
+    }
+    
+    public Query getQuery() {
+        return this.query;
+    }
+    
+    public ResultSet getResults() {
+        return this.results;
+    }
+
+    @Override
+    public GraffitiMessage clone() {
+        return new GraffitiMessage(this.query.clone(), this.results.clone());
+    }
+    
+    @Override
+    public void readFields(DataInput input) throws IOException {
+        this.query = new Query();
+        this.query.readFields(input);
+        this.results = new ResultSet();
+        this.results.readFields(input);
+    }
+
+    @Override
+    public void write(DataOutput output) throws IOException {
+        this.query.write(output);
+        this.results.write(output);
+    }
+}
---
+++ src/main/java/org/acaro/graffiti/processing/ResultSet.java
@@ -0,0 +1,52 @@
+package org.acaro.graffiti.processing;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Stack;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+
+public class ResultSet implements Writable, Cloneable {
+
+    private Stack<Text> results = new Stack<Text>();    
+
+    public ResultSet() { }
+    
+    protected ResultSet(Stack<Text> results) {
+        this.results = results;
+    }
+
+    public Text pop() {
+        return results.pop();
+    }
+    
+    public Text push(Text item) {
+        return results.push(item);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public ResultSet clone() {
+        return new ResultSet((Stack<Text>) results.clone());
+    }
+    
+    @Override
+    public void readFields(DataInput input) throws IOException {
+        int n = input.readInt();
+        for (int i = 0; i < n; i++) {
+            Text t = new Text();
+            t.readFields(input);
+            results.add(t);
+        }
+    }
+
+    @Override
+    public void write(DataOutput output) throws IOException {
+        output.writeInt(results.size());
+        for (Text result: results) {
+            result.write(output);
+        }
+    }
+}
